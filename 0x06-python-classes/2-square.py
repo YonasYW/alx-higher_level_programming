835bcb5a344208560c4fe51531c074a05b319d3f
Yonas yalew <yonasyalewwerke@gmail.com>
2024-01-23 04:53:13 -0800

    task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
+"""A module for class Square"""
+
+
 class Square:
-    """ A class that defines a square by its size
-    """
+    """The class Square"""
+
     def __init__(self, size=0):
-        """ Method to initialize the square object
-        """
+        """Initializer or constructor of new object"""
+
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         else:
-            self.__size = int(size)
+            self.__size = size

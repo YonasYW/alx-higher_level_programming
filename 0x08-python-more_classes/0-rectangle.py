b34cb411b1d3df1e499bbfeb0a30ae9601f10edb
Yonas yalew <yonasyalewwerke@gmail.com>
2024-01-29 02:10:31 -0800

    task 0

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
+"""Defines an empty Rectangle class using the pass keyword"""
 
 
 class Rectangle:
-    """Rectangle class."""
+    """Represent a rectangle."""
     pass
-
-
-if __name__ == '__main__':
-    my_rectangle = Rectangle()
-    print(type(my_rectangle))
-    print(my_rectangle.__dict__)
